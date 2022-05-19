3d70a7feaa073b99725b48bc0e641ae5d88dacc6
Kevin <rkvnh032gmail.com>
2022-05-18 18:15:42 -0700

    second

--- components/Board.js
+++ components/Board.js
@@ -4,23 +4,67 @@ class Board extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            grid: [],
+            history: []
         }
     }
-
-    createNodes() {
+    
+    componentDidMount() {
         const width = 48;
         const height = 24;
 
         const grid = [];
         for (let index = 0; index < width * height; index++) {
-            grid.push(index);
+            grid.push({index, visited: false});
         }
 
+        this.setState({
+            grid: grid
+        })
+    }
+
+    componentDidUpdate() {
+        
+    }
+
+    createNodes() {
         const nodes = grid.map((node) =>
             <div className={"node " + "node-" + node}></div>
         )
         return nodes
+    }
+
+    
+    bfs (graph, start) {
+        const moves = [-1, -48, 1, 48]
+
+        var queue = [];
+        queue.push(start);
+
+        var visited = [];
+        visited[start] = true;
+
+        var distances = [];
+        distances[start] = 0;
+
+        while (queue.length > 0) {
+            var node = queue.shift();
+
+            for (var i = 0; i < 4; i++) {
+                const nextNode = node + moves[i]
+                if (graph[nextNode] && !visited[nextNode]) {
+                    // Finish when destiny is found. Restart bfs for every new destiny
+                    // For each 4 moves iteration, update state
+                    this.setState({
+                        history: this.state.history.concat([])
+                    })
+                    visited[nextNode] = true;
+                    distances[nextNode] = distances[node] + 1;
+                    queue.push(nextNode);
+                }
+            }
+        }
+        return distances;
     }
 
     render() {
